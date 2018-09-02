6362fff0b77a0754a488cbee386947f355ae4d0c
ScrublordBG <megata10@gmail.com>
2018-09-02 22:08:27 +0300

    unpaidbills payment

--- static/js/client/home.js
+++ static/js/client/home.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   
     /* DataTables https://datatables.net/manual/installation */
-    $('table').DataTable();
+    var unpaidTable = $('#unpaid-bills-table').DataTable();
+
+    
+  $.ajaxSetup({
+    beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' +  localStorage.getItem("token"));
+    }
+  });
 
     $(function() {
       $('#subscribers-table-container, #bills-table-container, #top-10-subscribers-table-container, #paid-bills-table-container')
@@ -17,6 +24,46 @@ $(document).ready(function() {
     });
   
     $("#billsButton").click(function(ev){
+
+      $.ajax({
+        // crossOrigin: true,
+        // crossDomain: true,
+         type: 'GET',
+         xhrFields: { withCredentials: false },
+         url: "http://localhost:8080/api/client/bills/unpaid",
+         contentType: "application/json",
+         dataType: "json",
+         success: function (data) {
+             unpaidTable.clear();
+             //alert(data);
+
+                 $.each(data, function (i) {
+                   unpaidTable.row.add([
+
+                       data[i].billId, 
+                       data[i].service.name, 
+                       data[i].subscriber.phoneNumber, 
+                       data[i].subscriber.firstName +" "+ data[i].subscriber.lastName,
+                       data[i].startDate,
+                       data[i].endDate,
+                       data[i].amount,
+                       (data[i].amount * data[i].currency.exchangeRate).toFixed(2),
+                       data[i].currency.currency,
+                       data[i].paymentDate
+
+                   ]).draw(false);
+                  
+                 });
+         },
+         error: function () {
+             console.log("Unsuccessful request");
+         }
+     });
+
+
+
+
+
       ev.preventDefault;
       $('#subscribers-table-container').hide();
       $('#paid-bills-table-container').hide();
@@ -33,10 +80,45 @@ $(document).ready(function() {
     });
 
     $("#paidBillsButton").click(function(ev){
+
         ev.preventDefault;
         $('#subscribers-table-container').hide();
         $('#bills-table-container').hide();
         $('#top-10-subscribers-table-container').hide();
         $('#paid-bills-table-container').show();
+
+    });
+
+    $('#unpaid-bills-table tbody').on( 'click', 'tr', function () {
+      $(this).toggleClass('selected');
+  } );
+
+    $("#payBill").click(function(){
+      var billData = unpaidTable.rows('.selected').data()
+      var newData=[];
+      $.each(billData,function(i){
+        newData.push(billData[i][0]);
       });
+
+      $.ajax({
+        type: 'PUT',
+        xhrFields: { withCredentials: false },
+        url: 'http://localhost:8080/api/client/bills/pay',
+        contentType: "application/json",
+        data: JSON.stringify(newData),
+
+        success: function(data) {
+          alert('Load was performed.');
+          location.reload();
+        }
+      });
+
+
+
+    })
+
+
+
   });
+  
+ 
